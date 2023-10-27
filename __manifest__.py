75d8fb197ea34b866e1f99c81a5d4302ef8797bb
mgah77 <mauricio.gah@gmail.com>
2023-10-27 10:37:07 -0300

    crd

---
+++ __manifest__.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+
+{
+    'name': 'Insumar_check_credit',
+    'version': '1.0.01',
+    'category': 'General',
+    'summary': '',
+    'description': """
+
+
+       """,
+    'author' : 'M.Gah',
+    'website': '',
+    'depends': ['base','sale','account','l10n_cl_fe'],
+    'data': [	    
+        "views/res_partner.xml"
+    ],
+    'installable': True,
+    'auto_install': False,
+    'application': True,
+}
