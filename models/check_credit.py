6dccd057c045a9472a4a6b686e1e477baea11533
mgah77 <mauricio.gah@gmail.com>
2023-11-09 17:31:44 -0300

    Update check_credit.py

--- models/check_credit.py
+++ models/check_credit.py
@@ -1,7 +1,7 @@
 from odoo import models, fields, api, _
 
 
-class Add_credit(models.Model):
+class account_move_check_credit(models.Model):
     _inherit = ['account.move']
 
     notification_message = fields.Char(string="Mensajes", readonly=True)
@@ -32,7 +32,60 @@ class Add_credit(models.Model):
                     message = _("Este cliente sobrepasó su credito.")
                     self.notification_message = message
                     self.estado_partner = 'tope'  # Cambiar el estado
-                    self.state = 'cancel' 
+                    self.state = 'cancel'
+                    return {
+                        'warning': {
+                            'title': _('Aviso'),
+                            'message': message,
+                        }
+                    }
+                if pre_total > 0:
+                    message = _("Este cliente tiene facturas vencidas.")
+                    self.notification_message = message
+                    self.estado_partner = 'deudor'  # Cambiar el estado a "Deudor"
+                    return {
+                        'warning': {
+                            'title': _('Aviso'),
+                            'message': message,
+                        }
+                    }
+                else:
+                    self.estado_partner = 'active'  # Restaurar el estado a "Activo" si no hay facturas vencidas
+
+        self.notification_message = False
+
+class sale_order_check_credit(models.Model):
+    _inherit = ['sale.order']
+
+    notification_message = fields.Char(string="Mensajes", readonly=True)
+    credito = fields.Float(string="Credito")
+    estado_partner = fields.Selection([('active','Activo'),('deudor','Cliente Moroso'),('tope','Excede Credito')], string="Estado", default='active', readonly=True)
+
+    @api.onchange('partner_id')
+    def _check_partner_status(self):
+        if self.partner_id:
+            Invoice = self.env['account.move']
+            nuevo = Invoice.search_count([
+                ('partner_id', '=', self.partner_id.id),
+                ('move_type', '=', 'out_invoice'),
+                ('payment_state', '=', 'not_paid')
+            ])
+            if nuevo > 0:
+                pre_total = Invoice.search_count([# Revisa facturas por vencer
+                    ('partner_id', '=', self.partner_id.id),
+                    ('move_type', '=', 'out_invoice'),
+                    ('payment_state', '=', 'not_paid'),
+                    ('invoice_date_due', '<', fields.Date.today())
+                ])
+                vencido = Invoice.search([('partner_id', '=', self.partner_id.id),# Revisa facturas por vencidas
+                                        ('move_type', '=', 'out_invoice'),
+                                        ('payment_state', '=', 'not_paid')])
+                total_deuda = sum(factura.amount_total for factura in vencido)
+                if total_deuda > self.partner_id.credit_limit:
+                    message = _("Este cliente sobrepasó su credito.")
+                    self.notification_message = message
+                    self.estado_partner = 'tope'  # Cambiar el estado
+                    self.state = 'cancel'
                     return {
                         'warning': {
                             'title': _('Aviso'),
