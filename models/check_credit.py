1c3bd1ba41e1f1b41c9bfcdf7d29bd723c0ed6ff
mgah77 <mauricio.gah@gmail.com>
2023-11-09 16:26:41 -0300

    2

---
+++ models/check_credit.py
@@ -0,0 +1,10 @@
+from odoo import models, fields, api, _
+from odoo.exceptions import ValidationError
+
+
+class Add_credit(models.Model):
+    _inherit = ['account.move']
+
+    notification_message = fields.Char(string="Mensajes", readonly=True)
+    credito = fields.Float(string="Credito")
+    estado_partner = fields.Selection([('active','Activo'),('deudor','Cliente Moroso'),('tope','Excede Credito')], string="Estado", default='active', readonly=True)
